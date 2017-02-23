4723201a444b6d4bd078911f55e5d4dd42fd34de
msosborne3 <msosborne@ymail.com>
2017-02-22 21:30:16 -0600

    updated entries_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,8 @@
 Rails.application.routes.draw do
-  get 'entries/index'
-
-  get 'entries/show'
-
-  get 'entries/new'
-
-  get 'entries/create'
-
-  get 'entries/edit'
-
-  get 'entries/update'
-
-  get 'entries/destroy'
-
   devise_for :users
   root 'welcome#home'
+
+  resources :entries
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
