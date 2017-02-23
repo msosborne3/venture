912f77fd5ef87a8b79de78c0b1138bd2cf72497a
msosborne3 <msosborne@ymail.com>
2017-02-22 20:52:00 -0600

    added ActiveRecord associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :entries
+  has_many :followers, :class_name => 'Followings', :foreign_key => 'user_id'
+  has_many :following, :class_name => 'Followings', :foreign_key => 'follower_id' 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223024158) do
+ActiveRecord::Schema.define(version: 20170223024924) do
+
+  create_table "entries", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "followings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "follower_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
