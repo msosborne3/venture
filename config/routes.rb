fbbe6450d262d8877b793cd2b33b1098eb11e37b
msosborne3 <msosborne@ymail.com>
2017-02-22 20:54:04 -0600

    created entries controller using rails g controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'entries/index'
+
+  get 'entries/show'
+
+  get 'entries/new'
+
+  get 'entries/create'
+
+  get 'entries/edit'
+
+  get 'entries/update'
+
+  get 'entries/destroy'
+
   devise_for :users
   root 'welcome#home'
 
