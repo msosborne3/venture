02256500d6a6fe2f2a7c43e5a403eb1214977a59
msosborne3 <msosborne@ymail.com>
2017-02-23 23:34:34 -0600

    added comment model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223050349) do
+ActiveRecord::Schema.define(version: 20170224053146) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "entry_id"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entry_id"], name: "index_comments_on_entry_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "entries", force: :cascade do |t|
     t.integer  "user_id"
