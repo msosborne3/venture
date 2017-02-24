f1d29c83f69a56383a5b6851115cf24678089e0e
msosborne3 <msosborne@ymail.com>
2017-02-23 23:54:14 -0600

    Added comments to view

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'welcome#home'
 
-  resources :entries
+  resources :entries do
+    resources :comments
+  end
   resources :users
 
   post '/users/:id/follow_user', to: 'relationships#follow_user', as: :follow_user
   post '/users/:id/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
-
-  resources :comments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
