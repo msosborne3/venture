95c74f74c99b8c152ac561e1aa1944abd346e688
msosborne3 <msosborne@ymail.com>
2017-02-23 23:40:35 -0600

    added comments routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   post '/users/:id/follow_user', to: 'relationships#follow_user', as: :follow_user
   post '/users/:id/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
 
+  resources :comments
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
