fee758c9411282bdc140422a861bafe84ee8d611
msosborne3 <msosborne@ymail.com>
2017-02-25 15:14:37 -0600

    updated comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_attached_file :post_photo, default_url: ':style/no-image.png', styles: { medium: "236x300#" }
   validates_attachment_content_type :post_photo, content_type: /\Aimage\/.*\z/
 
+  # Returns the user who owns the place and therefore the post
   def post_user
     place = self.place
     place.user
