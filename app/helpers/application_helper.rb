8f7414894bf88d0d154b0cd4bd47af7a81847a73
msosborne3 <msosborne@ymail.com>
2017-02-25 15:11:39 -0600

    updated comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,14 +1,17 @@
 module ApplicationHelper
+  # Converts a date to 'Month day, year' for example 'February 23, 2017'
   def to_date(date)
     date.strftime("%B %e, %Y")
   end
 
+  # returns the number of followers a user has. For example '86 followers'
   def display_followers(user_id)
     user = User.find_by(id: user_id)
     num = user.followers.length
     return "#{num} followers"
   end
 
+  # returns the number of users that one is following. For example '103 following'
   def display_following(user_id)
     user = User.find_by(id: user_id)
     num = user.following.length
