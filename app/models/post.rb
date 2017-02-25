9415631eb76d80317ccc57fd63885323e26a2cf8
msosborne3 <msosborne@ymail.com>
2017-02-24 21:56:50 -0600

    added paperclip to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :place
 
-  has_attached_file :post_photo
+  has_attached_file :post_photo, styles: { medium: "236x300#" }
   validates_attachment_content_type :post_photo, content_type: /\Aimage\/.*\z/
 end
