5c33eef5295a2bbbb2ba17a01cb24d227611d61b
msosborne3 <msosborne@ymail.com>
2017-02-25 12:16:48 -0600

    route search bar to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,3 +1,5 @@
 class SearchController < ApplicationController
-
+  def search
+    @users = User.search(params[:search]).order("created_at DESC")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,6 @@ Rails.application.routes.draw do
   get '/users/:id/following', to: 'users#following', as: :following
   get '/users/:id/followers', to: 'users#followers', as: :followers
   get '/users/:id/journal', to: 'users#journal', as: :journal
+  get '/search', to: 'search#search', as: :search
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
