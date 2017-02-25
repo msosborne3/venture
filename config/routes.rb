217ac0a6966c33882a8c49a122251541b311d7be
msosborne3 <msosborne@ymail.com>
2017-02-24 22:25:09 -0600

    nested posts in places

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'posts/show'
-
-  get 'posts/index'
-
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'welcome#home'
 
@@ -21,7 +7,9 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
-  resources :places
+  resources :places do
+    resources :posts
+  end
 
   post '/users/:id/follow_user', to: 'relationships#follow_user', as: :follow_user
   post '/users/:id/unfollow_user', to: 'relationships#unfollow_user', as: :unfollow_user
