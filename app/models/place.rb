7da9f676ad55b8914c805ee2ca3c57a80e1c9258
msosborne3 <msosborne@ymail.com>
2017-02-25 12:21:52 -0600

    added search method

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,9 @@
 class Place < ApplicationRecord
   has_many :posts
   belongs_to :user
+
+  # Finds place where search term matches name
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%") 
+  end
 end
