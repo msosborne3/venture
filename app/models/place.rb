098c327e764acb13d70d38a3a43aa3f7e8102111
msosborne3 <msosborne@ymail.com>
2017-02-24 20:57:36 -0600

    added place and post associations

---
+++ app/models/place.rb
@@ -0,0 +1,4 @@
+class Place < ApplicationRecord
+  has_many :posts
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224174450) do
+ActiveRecord::Schema.define(version: 20170225025436) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,6 +35,21 @@ ActiveRecord::Schema.define(version: 20170224174450) do
     t.integer  "follower_id",  null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "places", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "place_id"
+    t.string   "link"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
