64394fdf0a8ba87f4fb4af0fabd3fab7316a7f43
msosborne3 <msosborne@ymail.com>
2017-02-24 21:53:08 -0600

    used paperclip to give posts a photo

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :place
+
+  has_attached_file :post_photo
+  validates_attachment_content_type :post_photo, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225025436) do
+ActiveRecord::Schema.define(version: 20170225035125) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -48,8 +48,12 @@ ActiveRecord::Schema.define(version: 20170225025436) do
     t.integer  "place_id"
     t.string   "link"
     t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "post_photo_file_name"
+    t.string   "post_photo_content_type"
+    t.integer  "post_photo_file_size"
+    t.datetime "post_photo_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
