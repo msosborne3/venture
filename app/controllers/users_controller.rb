a05dda9aa4300e804cafae1fa76bae7fb9ae68e1
msosborne3 <msosborne@ymail.com>
2017-02-25 14:56:34 -0600

    updated comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :following, :followers, :journal, :places]
 
+  # Displays all users
   def index
     @users = User.all
-    if params[:search]
-      @users = User.search(params[:search]).order("created_at DESC")
-    else
-      @users = User.all.order("created_at DESC")
-    end
   end
   
   # Displays a user's profile page
@@ -15,20 +11,24 @@ class UsersController < ApplicationController
     @places = @user.places.order("created_at asc")
   end
 
+  # displays a page of users that the given user is following
   def following
     @following = @user.following
   end
 
+  # displays a page of users that are following a given user
   def followers
     @followers = @user.followers
   end
 
+  # returns all of a user's journal entries
   def journal
     @entries = @user.entries.order("created_at desc")
   end
 
   private
 
+  # Finds the specific user from the id sent through params
   def set_user
     @user = User.find(params[:id])
   end
