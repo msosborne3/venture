a37047f846c32686ac193037777125ef298426e3
msosborne3 <msosborne@ymail.com>
2017-02-27 12:50:05 -0600

    added comments specs

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -5,9 +5,14 @@ RSpec.describe Entry, type: :model do
     @user = User.create
     @entry = Entry.create
     @user.entries << @entry
+    @entry.comments.build()
   end
 
   it "belongs to a user" do
     expect(@entry.user).to eq(@user)
+  end
+
+  it "has many comments" do
+    expect(@entry.comments.length).to eq(1)
   end
 end
