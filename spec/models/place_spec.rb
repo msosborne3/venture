dbb5e5946d34f23e5854cbf942db55d6c10c85df
msosborne3 <msosborne@ymail.com>
2017-03-04 01:23:05 -0600

    Add #search specs to Place class

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe Place, type: :model do
     @place.posts.create
     expect(@place.posts.length).to eq(1)
   end
+
+  # Tests the search class method of Place class.
+  describe '#search' do
+    before do
+      @place1 = Place.create(name: "Greece")
+      @place2 = Place.create(name: "New York")
+      @user.places << [@place1, @place2]
+    end
+
+    # Tests that if no places match the search, no places will be returned
+    it 'does not return any places if none match' do
+      search = Place.search('America')
+      expect(search.count).to eq(0)
+    end
+
+    # Tests that if a place does match the search, it returns ONLY the place
+    it 'returns only places that match search terms' do
+      search = Place.search('Greece')
+      expect(search).to include(@place1)
+      expect(search).to_not include(@place2)
+    end
+  end
 end
