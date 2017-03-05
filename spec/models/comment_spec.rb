4a7859f2a782bc752448c4cc1143f2d58cc3adc9
msosborne3 <msosborne@ymail.com>
2017-03-04 22:28:31 -0600

    Refactor comment_spec using shoulda gem

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,17 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  before(:each) do
-    @user = User.create(first_name: "Rory", last_name: 'Gilmore', email: 'rory@starshollow.com', password: 'logan4lyfe', password_confirmation: 'logan4lyfe')
-    @entry = @user.entries.build()
-    @comment = @entry.comments.build(user_id: @user.id)
-  end
+  
+  it { should belong_to(:user) }
 
-  it "belongs to a user" do
-    expect(@comment.user).to eq(@user)
-  end
+  it { should belong_to(:entry) }
 
-  it "belongs to entry" do
-    expect(@comment.entry).to eq(@entry)
-  end
 end
