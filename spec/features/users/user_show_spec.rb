e3fe106303bb66f43adb5b94ea9ff39f2c488043
msosborne3 <msosborne@ymail.com>
2017-03-05 02:06:10 -0600

    Refactor code using before

--- spec/features/users/user_show_spec.rb
+++ spec/features/users/user_show_spec.rb
@@ -3,44 +3,50 @@ require 'rails_helper'
 RSpec.feature 'User profile page', :type => :feature do
   let(:user) { FactoryGirl.create(:user) }
 
+  before(:each) { visit user_path(user) }
+
   scenario 'has link to journal' do
-    visit user_path(user)
     click_link 'Journal'
     expect(current_path).to eq(journal_path(user))
   end
 
   scenario 'has link to places' do
-    visit user_path(user)
     click_link 'Places'
     expect(current_path).to eq(user_path(user))
   end
 
   scenario "displays that user has no places if none exist" do
-    visit user_path(user)
     expect has_content?("no places")
   end
 
   scenario "lists user's places" do
     place = FactoryGirl.create(:place, user: user)
-    visit user_path(user)
     expect has_content?(place.name)
   end
+
+  scenario 'links to followers' do
+    expect has_link? followers_path(user)
+  end
+
+  scenario 'links to following' do
+    expect has_link? following_path(user)
+  end
+
 
   context 'logged in' do
 
     before do
       sign_in(user.email, user.password)
+      visit user_path(user)
     end
 
     context 'own page' do
       scenario 'has link to edit profile' do
-        visit user_path(user)
         click_link 'Edit Profile'
         expect(current_path).to eq(edit_user_registration_path)
       end
 
       scenario 'has link to create new place' do
-        visit user_path(user)
         expect has_link? new_place_path
       end
     end
@@ -48,32 +54,44 @@ RSpec.feature 'User profile page', :type => :feature do
     context "other user's page" do
       let(:user_2) { FactoryGirl.create(:user) }
 
+      before { visit user_path(user_2) }
+
       scenario 'has link to follow user if not following' do
-        visit user_path(user_2)
         expect has_link? 'Follow'
       end
 
       scenario 'does not have unfollow link if user is not following' do
-        visit user_path(user_2)
         expect has_no_link? 'Unfollow'
       end
 
       scenario 'has link to unfollow user if already following' do
-        visit user_path(user_2)
         click_link 'Follow'
         expect has_link? 'Unfollow'
       end
 
       scenario 'does not have follow link if user is already following' do
-        visit user_path(user_2)
         click_link 'Follow'
         expect has_no_link? 'Follow'
       end
 
       scenario 'does not have link to add new place' do
-        visit user_path(user_2)
         expect has_no_link? new_place_path
       end
+    end
+  end
+
+  context 'not logged in' do
+
+    scenario 'does not have follow link' do
+      expect has_no_link? 'Follow'
+    end
+
+    scenario 'does not have unfollow link' do
+      expect has_no_link? 'Unfollow'
+    end
+
+    scenario 'does not have link to add new place' do
+      expect has_no_link? new_place_path
     end
 
   end
