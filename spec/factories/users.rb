7135216640a3d88b2b00fc89ff54e780d56a8e3b
msosborne3 <msosborne@ymail.com>
2017-03-04 20:28:16 -0600

    Add Factory girl user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,10 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user do
-    sequence(:first_name) {|i| "FirstName#{i}"}
-    sequence(:last_name) {|i| "LastName#{i}"}
-    sequence(:email) {|i| "test#{i}@example.com"}
-    password "secret"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    password { Faker::Internet.password(8) }
   end
 end
