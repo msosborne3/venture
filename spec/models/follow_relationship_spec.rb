16757f044f08b15de1707fd392e13b94a41d6ac3
msosborne3 <msosborne@ymail.com>
2017-03-04 22:24:20 -0600

    Refactor follow_relationship_spec by removing before(:each)

--- spec/models/follow_relationship_spec.rb
+++ spec/models/follow_relationship_spec.rb
@@ -1,11 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FollowRelationship, type: :model do
-  before(:each) do
-    @user1 = User.create
-    @user2 = User.create
-    @relationship = FollowRelationship.create(follower_id: @user1.id, following_id: @user2.id)
-  end
   
   it {should belong_to(:follower) }
 
