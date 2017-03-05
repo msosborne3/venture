cca03d01803a8d3285f7698f99a6c40cc4085469
msosborne3 <msosborne@ymail.com>
2017-03-04 23:15:28 -0600

    Refactor post_spec using factories

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 # Tests the basic associations of the user model.
-RSpec.describe Place, type: :model do
-  let(:user) { User.create(first_name: "Rory", last_name: 'Gilmore', email: 'rory@starshollow.com', password: 'logan4lyfe', password_confirmation: 'logan4lyfe') }
-  let(:place) { user.places.build }
-  subject(:post) { place.posts.build }
+RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryGirl.create(:post)
+    @place = @post.place
+    @user = @place.user
+  end
 
   it { should belong_to(:place) }
 
@@ -14,7 +16,7 @@ RSpec.describe Place, type: :model do
     # Tests that the post_user method returns the users who has the post
     # through the place
     it "returns the user who owns the place that owns the post" do
-      expect(post.post_user).to be(user)
+      expect(@post.post_user).to be(@user)
     end
   end
 end
