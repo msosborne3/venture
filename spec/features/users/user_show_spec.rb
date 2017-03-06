4857695620069efdbb9fcf514c649f3d66e092d2
msosborne3 <msosborne@ymail.com>
2017-03-05 23:21:33 -0600

    Add places content to user_show_spec

--- spec/features/users/user_show_spec.rb
+++ spec/features/users/user_show_spec.rb
@@ -49,6 +49,32 @@ RSpec.feature 'User profile page', :type => :feature do
       scenario 'has link to create new place' do
         expect has_link? new_place_path
       end
+
+      context 'places' do
+        scenario 'can create a new place' do
+          click_on 'new-place'
+          fill_in 'Name', with: 'Greece'
+          click_on 'Create Place'
+          expect(user.places.count).to eq(1)
+        end
+
+        scenario 'clicking place shows posts' do
+          pending
+          place = FactoryGirl.create(:place, name: 'America')
+          visit user_path(user)
+          click_on place_path(place)   
+          expect has_content? 'America'       
+        end
+      end
+
+      scenario 'can create a new journal entry' do
+        click_on 'Journal'
+        click_on 'New Journal Entry'
+        fill_in 'Title', with: 'This is a cool title!'
+        fill_in 'Content', with: 'and some pretty cool content to match my title!'
+        click_on 'Create Entry'
+        expect(user.entries.count).to eq(1)
+      end
     end
 
     context "other user's page" do
@@ -61,6 +87,9 @@ RSpec.feature 'User profile page', :type => :feature do
       end
 
       scenario 'does not have unfollow link if user is not following' do
+        click_link 'Follow'
+        visit user_path(user_2)
+        click_link 'Unfollow' # making sure unfollow is clicked at least once
         expect has_no_link? 'Unfollow'
       end
 
