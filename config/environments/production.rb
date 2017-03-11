fb28bf21d258732c5e15f2881e9f1b1f39f57e4f
msosborne3 <msosborne@ymail.com>
2017-03-11 00:31:02 -0600

    Change environment variable from AWS_S3_BUCKET to BUCKET_NAME

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
     storage: :s3,
     s3_region: ENV["AWS_REGION"],
     s3_credentials: {
-      bucket: ENV['AWS_S3_BUCKET'],
+      bucket: ENV['BUCKET_NAME'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
