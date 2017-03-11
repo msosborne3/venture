47aafff6e2bcc02e29568dab1209a2c4c938dc94
msosborne3 <msosborne@ymail.com>
2017-03-11 00:24:38 -0600

    Add AWS_S3_REGION to paperclip production configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    s3_region: ENV["AWS_S3_REGION"],
     s3_credentials: {
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
