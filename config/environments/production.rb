9582c8f7e050d1ea3d83af1e9c44fa37e8a9528a
msosborne3 <msosborne@ymail.com>
2017-03-28 00:20:33 -0500

    Change host url to venture-away.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
     }
   }
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_controller.default_url_options = { host: 'venture-far.com' }
+  config.action_controller.default_url_options = { host: 'venture-away.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
